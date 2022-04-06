f7534ce0c02a3f52f7414a663a280fb3ec0b0e29
d4u0ul <ahb@ecomp.poli.br>
2022-04-05 23:53:32 -0300

    adição da função cadastro que dá origem a view da sub rota auth/cadastro

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 # Create your views here.
+
+def cadastro(request):
+    return HttpResponse('cadastro') # quando eu acessar no meu navegador a rota 'auth/cadastro', ele deve me retornar a resposta 'cadastro'  
 
