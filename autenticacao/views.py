7d50e713f661dd868f94246523a0fb795a69cca0
d4u0ul <ahb@ecomp.poli.br>
2022-04-10 15:23:11 -0300

    modificação do cadastro para dar um response de um autenticacao/templates/cadastro

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+'''
+arquivo views, onde vai ficar a lógica da nossa aplicação, onde fica toda aquela parte onde recebemos uma request e retornamos uma response, logo pegar o nome do usuário e verificar se ele existe, ou um e-mail/senha válido/inválido todo esse processamento fica aqui.
+'''
 
 def cadastro(request):
-    return HttpResponse('cadastro') # quando eu acessar no meu navegador a rota 'auth/cadastro', ele deve me retornar a resposta 'cadastro'  
+    return render(request, 'cadastro.html')
 
+def login(request):
+    return HttpResponse('login') # quando eu acessar no meu navegador a rota 'auth/login', ele deve me retornar a resposta 'login'
--- freewlaway/urls.py
+++ freewlaway/urls.py
@@ -18,6 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('autenticacao.urls'))
-    path('login/', include('login.urls'))
+    path('auth/', include('autenticacao.urls')),
 ]
