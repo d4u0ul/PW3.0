ea8e4a46280c82f87620b66c5af3a1b632b9d07e
d4u0ul <ahb@ecomp.poli.br>
2022-04-10 19:49:00 -0300

    add de validações de senha=confirmarsenha, user/senha !=vazio, user já existir

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,7 +8,22 @@ arquivo views, onde vai ficar a lógica da nossa aplicação, onde fica toda aqu
 '''
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('password')
+        confirmar_senha = request.POST.get('confirm-password')
+        #print (f"{username}|{senha}|{confirmar_senha}")
+        if not senha == confirmar_senha:
+            return redirect("/auth/cadastro")
+        if len(username.strip())==0 or len(senha.strip())==0:
+            return redirect("/auth/cadastro")
+        user = User.objects.filter(username = username)
+        if user.exists:
+            return redirect("/auth/cadastro")
+            
+        return HttpResponse('Recebido')
 
 def login(request):
     return HttpResponse('login') # quando eu acessar no meu navegador a rota 'auth/login', ele deve me retornar a resposta 'login'
