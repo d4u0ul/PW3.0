3b1e261cd66526e6c5c3ce2f126ee5ef3f092fe5
d4u0ul <ahb@ecomp.poli.br>
2022-04-11 00:11:13 -0300

    add redirects da página autenticacao/login.html

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib import auth
 # Create your views here.
 '''
 arquivo views, onde vai ficar a lógica da nossa aplicação, onde fica toda aquela parte onde recebemos uma request e retornamos uma response, logo pegar o nome do usuário e verificar se ele existe, ou um e-mail/senha válido/inválido todo esse processamento fica aqui.
@@ -49,6 +50,20 @@ def cadastro(request):
 
 def login(request):
     #return HttpResponse('login') # quando eu acessar no meu navegador a rota 'auth/login', ele deve me retornar a resposta 'login'
+    if request.method == "GET":
+        return render(request,"login.html")
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        senha = request.POST.get("password")
+        
+        usuario = auth.authenticate(username=username, password=senha)
 
-    return render(request,"login.html")
+        if not usuario:
+            messages.add_message(request,constants.ERROR, 'usuário ou senha inválidos')
+            return redirect("/auth/login")
+        else:
+            auth.login(request, usuario)
+            return redirect("/plataforma")
+
+         
 
